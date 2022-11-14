f0ff13463c97beea2a654a4219bb446c61933ab8
Chethan T B <chethan171856@gmail.com>
2022-11-14 15:12:33 +0530

    second commit

--- QrScanner/app/src/main/java/com/chethan/qrcodescanner/Screens.kt
+++
@@ -1,7 +0,0 @@
-package com.chethan.qrcodescanner
-
-sealed class Screens(val route : String) {
-    object HomeScreen : Screens("home_screen")
-    object ScannerScreen : Screens("scanner_screen")
-    object ValidateScreen : Screens("validate_screen")
-}
